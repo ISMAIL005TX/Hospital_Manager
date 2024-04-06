4d96002c028d74ddad4b02b88deb2aa2b98496f9
ISMAIL005TX <haninismail030@gmail.com>
2024-04-06 16:26:02 +0000

    creation de projet

---
+++ Hospital_Manager/Struct.c
@@ -0,0 +1,9 @@
+struct HOSPITAL_PATIENT
+{
+    int PATIENT_ID;
+    char PATIENT_NAME[50];
+    char PATIENT_ADRESS[100];
+    int PATIENT_AGE;
+    char PATIENT_SEX;
+    char PATIENT_ILLNESS[200];
+};
---
+++ Hospital_Manager/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include
+
+int main()
+{
+    int MENU;
+    /// Menu pour choisir l'operation a executer
+
+    printf("1) AJOUTER UN NOUVEAU PATIENT");
+    printf("\n2) RECHERCHER OU MODIFIER LE DOSSIER D'UN PATIENT");
+    printf("\n3) ACCEDEZ A LA LISTE DES DOSSIERS DES PATIENTS");
+    printf("\n4) SUPPRIMER LE DOSSIER D'UN PATIENT\n");
+    printf("\nVEILLEZ CHOISIR UNE OPTION :\t");
+    scanf("%d",&MENU);
+}
